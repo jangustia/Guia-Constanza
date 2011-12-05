afcef7203ba5d38cf4f59dde3bdae3f9fe773ae3
Waldemar Figueroa Medina <waldemar@nobox.com>
2011-12-04 22:14:49 -0400

    New comments template code
    Signed-off-by: Waldemar Figueroa Medina <waldemar@nobox.com>

--- wp-content/themes/guiaconstanza/comments.php
+++ wp-content/themes/guiaconstanza/comments.php
@@ -13,33 +13,14 @@
  */
 ?>
 
-<?php if ( post_password_required() ) : ?>
-				<p><?php _e( 'This post is password protected. Enter the password to view any comments.', 'boilerplate' ); ?></p>
-<?php
-		/* Stop the rest of comments.php from being processed,
-		 * but don't kill the script entirely -- we still have
-		 * to fully load the template.
-		 */
-		return;
-	endif;
-?>
-
 <?php
 	// You can start editing here -- including this comment!
 ?>
 
 <?php if ( have_comments() ) : ?>
+		<section id="comments">
 			<!-- STARKERS NOTE: The following h3 id is left intact so that comments can be referenced on the page -->
-			<h3 id="comments-title"><?php
-				printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'boilerplate' ),
-				number_format_i18n( get_comments_number() ), '' . get_the_title() . '' );
-			?></h3>
-
-<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
-				<?php previous_comments_link( __( '&larr; Older Comments', 'boilerplate' ) ); ?>
-				<?php next_comments_link( __( 'Newer Comments &rarr;', 'boilerplate' ) ); ?>
-<?php endif; // check for comment navigation ?>
-
+			<h2 id="comments-title"><?php __('Comments', 'boilerplate') ?></h2>
 			<ol>
 				<?php
 					/* Loop through and list the comments. Tell wp_list_comments()
@@ -70,3 +51,4 @@
 <?php endif; // end have_comments() ?>
 
 <?php comment_form(); ?>
+		</section><!-- #comments -->
