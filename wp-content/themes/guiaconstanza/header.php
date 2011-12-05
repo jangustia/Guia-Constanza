d12b421b034676d90751cee823563b2d2a0be5b2
Waldemar Figueroa Medina <waldemar@nobox.com>
2011-12-04 22:13:52 -0400

    New header template code
    Signed-off-by: Waldemar Figueroa Medina <waldemar@nobox.com>

--- wp-content/themes/guiaconstanza/header.php
+++ wp-content/themes/guiaconstanza/header.php
@@ -60,12 +60,16 @@
 			</div><!-- #header_info -->
 		</div><!-- .wrapper -->
 	</header><!-- close header -->
-
+	<?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
+	<a class="visuallyhidden" id="skip" href="#content" title="<?php esc_attr_e( 'Skip to content', 'boilerplate' ); ?>"><?php _e( 'Skip to content', 'boilerplate' ); ?></a>
 	<nav class="wrap" role="navigation">
-	  <?php /*  Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-		<a id="skip" href="#content" title="<?php esc_attr_e( 'Skip to content', 'boilerplate' ); ?>"><?php _e( 'Skip to content', 'boilerplate' ); ?></a>
 		<?php /* Our navigation menu.  If one isn't filled out, wp_nav_menu falls back to wp_page_menu.  The menu assiged to the primary position is the one used.  If none is assigned, the menu with the lowest ID is used.  */ ?>
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+		<?php
+			wp_nav_menu( array( 
+				'menu' => 'main',
+				'menu_class' => '',
+				'container' => false,
+				)
+			);
+		?>
 	</nav><!-- #access -->
-
-	<section id="content" role="main">
