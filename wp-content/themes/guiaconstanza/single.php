9376a125a1fe1b0779e77b8d1203125071bb5891
Waldemar Figueroa Medina <waldemar@nobox.com>
2011-12-04 22:15:40 -0400

    Added most static content to single.php template file
    - This is used to display one single place's info.
    
    Signed-off-by: Waldemar Figueroa Medina <waldemar@nobox.com>

--- wp-content/themes/guiaconstanza/single.php
+++ wp-content/themes/guiaconstanza/single.php
@@ -9,39 +9,110 @@
 
 get_header(); ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-				<nav id="nav-above" class="navigation">
-					<?php previous_post_link( '%link', '' . _x( '&larr;', 'Previous post link', 'boilerplate' ) . ' %title' ); ?>
-					<?php next_post_link( '%link', '%title ' . _x( '&rarr;', 'Next post link', 'boilerplate' ) . '' ); ?>
-				</nav><!-- #nav-above -->
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1><?php the_title(); ?></h1>
-					<div class="entry-meta">
-						<?php boilerplate_posted_on(); ?>
-					</div><!-- .entry-meta -->
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
-					</div><!-- .entry-content -->
-<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
-					<footer id="entry-author-info">
-						<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'boilerplate_author_bio_avatar_size', 60 ) ); ?>
-						<h2><?php printf( esc_attr__( 'About %s', 'boilerplate' ), get_the_author() ); ?></h2>
-						<?php the_author_meta( 'description' ); ?>
-						<a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
-							<?php printf( __( 'View all posts by %s &rarr;', 'boilerplate' ), get_the_author() ); ?>
-						</a>
-					</footer><!-- #entry-author-info -->
-<?php endif; ?>
-					<footer class="entry-utility">
-						<?php boilerplate_posted_in(); ?>
-						<?php edit_post_link( __( 'Edit', 'boilerplate' ), '<span class="edit-link">', '</span>' ); ?>
-					</footer><!-- .entry-utility -->
-				</article><!-- #post-## -->
-				<nav id="nav-below" class="navigation">
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'boilerplate' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'boilerplate' ) . '</span>' ); ?></div>
-				</nav><!-- #nav-below -->
-				<?php comments_template( '', true ); ?>
+				<div id="content" class="wrap" role="main">
+					<h1 id="subheader" class="wrap"><span><?php
+						$category = get_the_category();
+						echo $category[0]->cat_name;
+					?></span></h1>
+					<div id="details">
+						<ul class="breadcrumbs">
+							<li><a href="index.php">Inicio</a></li>
+							<li><a href="hoteles.php">Hoteles</a></li>
+							<li>Rancho Guaraguao</li>
+						</ul>
+						<h2 class="fn org">Rancho Guaraguao</h2>
+						<figure>
+							<span>Cabañas Palofino</span>
+							<img src="<?php bloginfo('template_url') ?>/images/rancho.jpg" alt="Fotografia de unos locales en Rancho Guaraguao" />
+						</figure>
+						<h3>Descripción</h3>
+						<p>Sed ut perspiciatis unde omnis iste natus error sit volupatetem accusantium doloremquede laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vita dicta sunt explicabo.</p>
+			
+						<h3>Incluye</h3>
+						<ul class="incluye">
+							<li class="tv">Televisión</li>
+							<li class="wifi not-inc">Wireless</li>
+							<li class="delivery not-inc">Delivery</li>
+							<li class="menu">Menu</li>
+							<li class="drink">Tragos</li>
+						</ul>
+			
+						<h3>Contacto</h3>
+						<div class="adr">
+							<div class="street-address">Calle Guarocuya No. 461</div>
+							<span class="locality">El Millon</span>, <span class="region">Santo Domingo</span>
+							<div class="country-name">Republica Dominicana</div>
+						</div>
+						<div class="tel">
+							<span class="type">Work</span> +1-809-530-6192
+						</div>
+						<div class="tel">
+							<span class="type">Fax</span> +1-809-530-6192
+						</div>
+						<div class="tel">
+							<span class="type">Constanza</span> +1-809-539-1553
+						</div>
+						<div>Email:
+							<span class="email">reservas@altocerro.com</span>
+						</div>
+						<div>Web:
+							<a rel="external" class="url" href="http://www.altocerro.com">Altocerro.com</a>
+						</div>
+			
+					<section class="map">
+						<h3>Ubicación</h3>
+						<figure>
+							<img src="img/Hotel_Mapa.png" alt="" />
+						</figure>
+					</section>
+			
+					<section class="ratings">
+						<h3>Calificar Hotel</h3>
+						<form>
+							<h4>Calificación:</h4>
+							<label for="excelente"><input name ="rating" id="excelente" type="radio" /> Excelente</label>
+							<label for="masomenos"><input name="rating" id="masomenos" type="radio" /> Más o Menos</label>
+							<label for="muybueno"><input name="rating" id="muybueno" type="radio" /> Muy Bueno</label>
+							<label for="terrible"><input name="rating" id="terrible" type="radio" /> Terrible</label>
+							<a href="#">Deja tu opinión</a>
+						</form>
+					</section>
+
+					<?php comments_template( '', true ); ?>
+				</div><!-- #details -->
+
+				<aside class="sideinfo">
+					<section class="box">
+						<h3>Recomendamos</h3>
+		
+						<img src="img/generic_thumb.png" alt="" />
+						<h4><a href="#">Nulla a tellus also magna imperdiet intomarte molestie vitae sed purus.</a></h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a tellus ac magnanmui perdiet molestie vitae sed purus. Nulla a tellus ac mangnanmui perdiet molestie vitae sed purus.</p>
+					</section>
+		
+					<section id="checkins" class="box">
+						<h3>Check-ins Recientes</h3>
+						<?php for($i=0;$i<3;$i++): ?>
+						<div>
+							<img src="<?php bloginfo('template_url') ?>/images/checkins_thumb.png" alt="" />
+							<span class="name">Danilo Jimenez @ <a href="#">Rancho Guaraguao</a></span>
+							<span class="origin">Santo Domingo - 2 days ago</span>
+						</div>
+						<?php endfor; ?>
+					</section>
+		
+					<section>
+						<h2>Atractivos</h2>
+						<?php for ($i=0;$i<6;$i++): ?>
+						<article>
+							<img src="img/smaller_thumb.png" alt="" />
+							<h4><a href="#">Nunc convallis lectus elementum diam sodales in suscipit</a></h4>
+						</article>
+						<?php endfor; ?>
+						<div class="skyscraper"></div>
+					</section>
+				</aside>
+
 <?php endwhile; // end of the loop. ?>
-<?php get_sidebar(); ?>
+			</div><!-- #content -->
 <?php get_footer(); ?>
