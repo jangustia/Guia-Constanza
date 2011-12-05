180260cb5e55001429d5619a84c0579a10ff1b6e
Waldemar Figueroa Medina <waldemar@nobox.com>
2011-12-04 22:17:01 -0400

    deleted wordpress message in footer
    - Still pending to do footer widgets
    
    Signed-off-by: Waldemar Figueroa Medina <waldemar@nobox.com>

--- wp-content/themes/guiaconstanza/footer.php
+++ wp-content/themes/guiaconstanza/footer.php
@@ -18,8 +18,6 @@
 	 */
 	get_sidebar( 'footer' );
 ?>
-		<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-		<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
 	</footer><!-- footer -->
 
 	<!-- JavaScript at the bottom for fast page loading -->
