20e3ad4fc5bb518fe16f88b08e5fb89e4cdb4c2d
Waldemar Figueroa Medina <waldemar@nobox.com>
2011-12-09 20:43:08 -0400

    Split "atractivos" sidebar to separate file
    Signed-off-by: Waldemar Figueroa Medina <waldemar@nobox.com>

---
+++ wp-content/themes/guiaconstanza/sidebar-atractivos.php
@@ -0,0 +1,12 @@
+					<div id="sidebar">
+						<section>
+							<h2>Atractivos</h2>
+							<?php for ($i=0;$i<4;$i++): ?>
+							<article>
+								<img src="img/generic_thumb.png" alt="" />
+								<h3><a href="#">Nunc convallis lectus elementum diam sodales in suscipit</a></h3>
+							</article>
+							<?php endfor; ?>
+							<div class="skyscraper"></div>
+						</section>
+					</div>
