16a79c80ae84a506d87959139989abb22f6635ac
Waldemar Figueroa Medina <waldemar@nobox.com>
2012-02-01 21:14:35 -0400

    Fixed buggy slider behavior.
    Signed-off-by: Waldemar Figueroa Medina <waldemar@nobox.com>

--- wp-content/themes/guiaconstanza/sidebar-featured.php
+++ wp-content/themes/guiaconstanza/sidebar-featured.php
@@ -4,25 +4,25 @@
 		'posts_per_page' => 5,
 		'orderby'        => 'rand'
 	));
-	
+
 	$bares_y_rests = new WP_Query (array (
 		'post_type'      => 'gc_bares_y_rests',
 		'posts_per_page' => 5,
 		'orderby'        => 'rand'
 	));
-	
+
 	$first_post = TRUE;
 ?>
 				<div id="featured" class="wrap">
 					<section id="featured_hotel">
 						<h2>Hoteles Destacados</h2>
-						
+
 						<div id="hotel_slide" class="niveSlider">
 						<?php while ($hoteles->have_posts()): $hoteles->the_post(); ?>
 							<img src="<?php bloginfo ('template_url'); ?>/images/img_hoteles.jpg" alt="<?php the_title(); ?>" title="#hotel_caption_<?php the_ID(); ?>" />
 						<?php endwhile; $hoteles->rewind_posts(); ?>
 						</div>
-						
+
 						<ul class="visuallyhidden">
 						<?php while ($hoteles->have_posts()): $hoteles->the_post(); ?>
 							<li id="hotel_caption_<?php the_ID(); ?>" class="nivo-html-caption">
@@ -32,23 +32,23 @@
 						<?php endwhile; ?>
 						</ul>
 					</section>
-					
+
 					<section id="featured_bars">
 						<h2>Bares y Restaurantes</h2>
-						
+
 						<nav class="featured_arrows">
 							<ul>
-								<li><a class="next" href="javascript:;">▶</a></li>
-								<li><a class="prev" href="javascript:;">◀</a></li>
+								<li><a class="next ir" href="javascript:;">Siguiente</a></li>
+								<li><a class="prev ir" href="javascript:;">Anterior</a></li>
 							</ul>
 						</nav>
-						
+
 						<?php while ($bares_y_rests->have_posts()): $bares_y_rests->the_post(); ?>
 							<div class="slide<?php if ($first_post) { $first_post = FALSE; ?> active<?php }?>">
 								<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 								<img src="<?php bloginfo('template_url') ?>/images/generic_thumb.png" alt="" />
 								<p><?php echo new_excerpt (120); ?></p>
-								
+
 								<ul class="incluye">
 								<?php foreach (bares_y_rests_icons() as $slug => $name): ?>
 									<li class="<?php echo $slug; ?>"><?php echo $name;?></li>
