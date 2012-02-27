5e52acd0090ac1d45af3ee81c9e20c4b414dc4b9
Waldemar Figueroa Medina <waldemar@nobox.com>
2012-02-26 21:19:42 -0400

    - Updated index markup for new design changes.

--- wp-content/themes/guiaconstanza/index.php
+++ wp-content/themes/guiaconstanza/index.php
@@ -4,7 +4,7 @@
  *
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query. 
+ * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
@@ -15,45 +15,48 @@
 
 	get_header();
 	get_sidebar( 'featured' );
-	
+
 	$query_args = array (
 		'post_type' => 'gc_atractivos'
 	);
-	
+
 	$atractivos = new WP_Query ($query_args);
 ?>
 
-				<div id="content" class="wrap" role="main">
-					<section id="atractivos">
-						<?php if (!$atractivos->have_posts()): ?>
-						<?php else: ?>
-							<h2>Atractivos</h2>
-							
-							<?php while ( $atractivos->have_posts() ) : $atractivos->the_post(); ?>
-								<article>
-									<?php the_post_thumbnail ('thumbnail'); ?>
-									<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-									<?php the_excerpt(); ?>
-								</article>
-							<?php endwhile; ?>
-							
-							<div class="fullbanner">
-								<script type="text/javascript"><!--
-									google_ad_client = "ca-pub-8397939586383038";
-									/* GuiaConstanza 468x60 */
-									google_ad_slot = "6293014751";
-									google_ad_width = 468;
-									google_ad_height = 60;
-									//-->
-								</script>
-							</div>
-						<?php endif; ?>
-					</section><!-- #atractivos -->
-					
-					<?php $atractivos->rewind_posts(); ?>
-					
-					<section id="dondeir-widget">
-						<h2>Donde ir</h2>
+		<div id="container" class="wrap" role="main">
+			<div id="main_content">
+				<?php if (!$atractivos->have_posts()): ?>
+				<?php else: ?>
+				<section class="places-list">
+					<h2>Atractivos</h2>
+
+					<?php while ( $atractivos->have_posts() ) : $atractivos->the_post(); ?>
+						<article>
+							<?php the_post_thumbnail ('thumbnail'); ?>
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							<?php the_excerpt(); ?>
+						</article>
+					<?php endwhile; ?>
+
+					<div class="fullbanner">
+						<script type="text/javascript"><!--
+							google_ad_client = "ca-pub-8397939586383038";
+							/* GuiaConstanza 468x60 */
+							google_ad_slot = "6293014751";
+							google_ad_width = 468;
+							google_ad_height = 60;
+							//-->
+						</script>
+					</div>
+				</section><!-- #atractivos -->
+				<?php endif; ?>
+			</div><!-- #main_content -->
+			<?php $atractivos->rewind_posts(); ?>
+
+			<div id="side_content">
+				<section id="dondeir-widget">
+					<h2>Donde ir</h2>
+					<div id="map_container">
 						<div id="dondeir_map"></div>
 						<ul id="marker_list" class="visuallyhidden">
 						<?php while ( $atractivos->have_posts() ) : $atractivos->the_post(); ?>
@@ -65,9 +68,11 @@
 								</li>
 							<?php endif; ?>
 						<?php endwhile; ?>
-						</ul>
-						<div class="verticalbanner"></div>
-						<div class="squarepopup"></div>
-					</section>
+						</ul><!-- #marker_list -->
+					</div><!-- #map_container -->
+					<div class="verticalbanner"></div>
+					<div class="squarepopup"></div>
+				</section>
+			</div><!-- #side_content -->
 
 <?php get_footer(); ?>
