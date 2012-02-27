7ecc2de663a7a3ecaa62484baee517f247f7752c
Waldemar Figueroa Medina <waldemar@nobox.com>
2012-02-26 21:15:50 -0400

    - Updated markup for category pages.

--- wp-content/themes/guiaconstanza/category.php
+++ wp-content/themes/guiaconstanza/category.php
@@ -8,14 +8,14 @@
  */
 
 	get_header();
-	
+
 	$category   = get_category (get_query_var ('cat'));
 	$has_search = in_array ($category->slug, array ('hoteles', 'bares-y-restaurantes'));
 	$query_args = array (
 		'post_type'      => post_type_for_category ($category->slug),
 		'posts_per_page' => 7
 	);
-	
+
 	if ($has_search)
 	{
 		$nombre = isset ($_REQUEST['nombre']) ? $_REQUEST['nombre'] : '';
@@ -23,17 +23,17 @@
 		$bar    = isset ($_REQUEST['bar'])    ? $_REQUEST['bar']    : FALSE;
 		$tv     = isset ($_REQUEST['tv'])     ? $_REQUEST['tv']     : FALSE;
 		$wifi   = isset ($_REQUEST['wifi'])   ? $_REQUEST['wifi']   : FALSE;
-		
+
 		$chim   = isset ($_REQUEST['chim'])  ? $_REQUEST['chim']  : FALSE;
 		$pool   = isset ($_REQUEST['pool'])  ? $_REQUEST['pool']  : FALSE;
 		$james  = isset ($_REQUEST['james']) ? $_REQUEST['james'] : FALSE;
 		$heat   = isset ($_REQUEST['heat'])  ? $_REQUEST['heat']  : FALSE;
 		$menu   = isset ($_REQUEST['menu'])  ? $_REQUEST['menu']  : FALSE;
-		
+
 		$crio     = isset ($_REQUEST['crio'])     ? $_REQUEST['crio']     : FALSE;
 		$inter    = isset ($_REQUEST['inter'])    ? $_REQUEST['inter']    : FALSE;
 		$delivery = isset ($_REQUEST['delivery']) ? $_REQUEST['delivery'] : FALSE;
-		
+
 		if (!empty ($nombre))
 			$query_args['meta_query'][] = array ('key' => 'nombre', 'value' => $nombre, 'compare' => 'LIKE');
 		if (!empty ($tipo))
@@ -44,7 +44,7 @@
 			$query_args['meta_query'][] = array ('key' => 'tv',     'value' => $tv);
 		if (!empty ($wifi))
 			$query_args['meta_query'][] = array ('key' => 'wifi',   'value' => $wifi);
-		
+
 		if (!empty ($chim)  && $category->slug == 'hoteles')
 			$query_args['meta_query'][] = array ('key' => 'chim',  'value' => $chim);
 		if (!empty ($pool)  && $category->slug == 'hoteles')
@@ -55,7 +55,7 @@
 			$query_args['meta_query'][] = array ('key' => 'heat',  'value' => $heat);
 		if (!empty ($menu)  && $category->slug == 'hoteles')
 			$query_args['meta_query'][] = array ('key' => 'menu',  'value' => $menu);
-		
+
 		if (!empty ($crio)     && $category->slug == 'bares-y-restaurantes')
 			$query_args['meta_query'][] = array ('key' => 'crio',     'value' => $crio);
 		if (!empty ($inter)    && $category->slug == 'bares-y-restaurantes')
@@ -63,7 +63,7 @@
 		if (!empty ($delivery) && $category->slug == 'bares-y-restaurantes')
 			$query_args['meta_query'][] = array ('key' => 'delivery', 'value' => $delivery);
 	}
-	
+
 	$loop = new WP_Query ($query_args);
 ?>
 
@@ -74,43 +74,45 @@
 				<?php // Get "Featured" section
 				if ($has_search)
 					get_sidebar( 'featured' ); ?>
-				
+
 				<?php if ($has_search): ?>
-					<div id="adv_search">
+					<div id="adv_search" class="wrap">
 						<form action="<?php echo get_category_link ($category->cat_ID) ?>">
 							<h2>Búsqueda Avanzada</h2>
 							<input type="text" id="name" name="nombre"
 								value="<?php echo $nombre ?>"
 								placeholder="Nombre"/>
-							
+
 							<?php get_template_part ('search', $category->slug); ?>
-							
-							<input type="submit" value="Buscar"/>
+
+							<input class="button" type="submit" value="Buscar"/>
 						</form>
 					</div>
 				<?php endif ?>
-				
-				<div id="content" class="wrap" role="main">
+
+				<div id="container" class="wrap" role="main">
+					<div id="main_content">
 					<?php if ( !$loop->have_posts() ): ?>
 						<h2>No hay nah, bro</h2>
 					<?php else: ?>
-						<div id="places-list">
+						<div class="places-list">
 							<h2><?php single_cat_title(); ?></h2>
 							<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 								<article>
-									<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 									<?php the_post_thumbnail ('thumbnail'); ?>
+									<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 									<p><?php the_excerpt(); ?></p>
 								</article>
 							<?php endwhile; ?>
-							</ul>
-						</div>
+						</div><!-- .places-list -->
 					<?php endif; ?>
-					
-					<div id="sidebar">
-						<?php // Get "Atractivos" section
-						get_sidebar( 'atractivos' ); ?>
-					</div>
-				</div>
+					</div><!-- #main_content -->
+
+					<div id="side_content">
+						<div class="places-list">
+							<?php // Get "Atractivos" section
+							get_sidebar( 'atractivos' ); ?>
+						</div><!-- .places-list -->
+					</div><!-- #side_content -->
 
 <?php get_footer(); ?>
